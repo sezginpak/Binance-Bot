68b7f205e32b85f0ff860fe4cc0a186421e8befa
sezgin <sezgintube64@gmail.com>
2019-03-01 18:34:09 +0300

    v0.1.8 --> Update...

--- mainwin.py
+++ mainwin.py
@@ -8,32 +8,34 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from config import x, y
-import DovizKurlari
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 200)
+        MainWindow.resize(400, 275)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.infobutton = QtWidgets.QPushButton(self.centralwidget)
-        self.infobutton.setGeometry(QtCore.QRect(60, 110, 101, 31))
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 401, 231))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
+        self.tabWidget.setSizePolicy(sizePolicy)
+        self.tabWidget.setFocusPolicy(QtCore.Qt.TabFocus)
+        self.tabWidget.setToolTipDuration(-1)
+        self.tabWidget.setObjectName("tabWidget")
+
+        self.tab1 = QtWidgets.QWidget()
+        self.tab1.setObjectName("tab1")
+
+        self.infobutton = QtWidgets.QPushButton(self.tab1)
+        self.infobutton.setGeometry(QtCore.QRect(40, 120, 101, 31))
         self.infobutton.setStyleSheet("")
         self.infobutton.setObjectName("infobutton")
-        self.maintext = QtWidgets.QLabel(self.centralwidget)
-        self.maintext.setGeometry(QtCore.QRect(130, 0, 131, 41))
-        font = QtGui.QFont()
-        font.setPointSize(18)
-        self.maintext.setFont(font)
-        self.maintext.setScaledContents(False)
-        self.maintext.setAlignment(QtCore.Qt.AlignCenter)
-        self.maintext.setWordWrap(False)
-        self.maintext.setObjectName("maintext")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(50, 60, 111, 21))
-        self.label.setObjectName("label")
-        self.inputcoininfo = QtWidgets.QLineEdit(self.centralwidget)
-        self.inputcoininfo.setGeometry(QtCore.QRect(50, 90, 121, 16))
+
+        self.inputcoininfo = QtWidgets.QLineEdit(self.tab1)
+        self.inputcoininfo.setGeometry(QtCore.QRect(30, 90, 121, 16))
         self.inputcoininfo.setInputMethodHints(QtCore.Qt.ImhLatinOnly|QtCore.Qt.ImhPreferUppercase|QtCore.Qt.ImhUppercaseOnly)
         self.inputcoininfo.setInputMask("")
         self.inputcoininfo.setText("")
@@ -42,10 +44,110 @@ class Ui_MainWindow(object):
         self.inputcoininfo.setReadOnly(False)
         self.inputcoininfo.setClearButtonEnabled(False)
         self.inputcoininfo.setObjectName("inputcoininfo")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(220, 90, 141, 31))
+
+        self.label = QtWidgets.QLabel(self.tab1)
+        self.label.setGeometry(QtCore.QRect(40, 60, 111, 21))
+        self.label.setObjectName("label")
+
+        self.maintext = QtWidgets.QLabel(self.tab1)
+        self.maintext.setGeometry(QtCore.QRect(30, 10, 121, 31))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.maintext.setFont(font)
+        self.maintext.setScaledContents(False)
+        self.maintext.setAlignment(QtCore.Qt.AlignCenter)
+        self.maintext.setWordWrap(False)
+        self.maintext.setObjectName("maintext")
+
+        self.label_2 = QtWidgets.QLabel(self.tab1)
+        self.label_2.setGeometry(QtCore.QRect(220, 70, 141, 31))
         self.label_2.setText("")
         self.label_2.setObjectName("label_2")
+        self.tabWidget.addTab(self.tab1, "")
+        self.tab2 = QtWidgets.QWidget()
+        self.tab2.setObjectName("tab2")
+
+        self.input_coin_trade = QtWidgets.QLineEdit(self.tab2)
+        self.input_coin_trade.setGeometry(QtCore.QRect(60, 90, 121, 16))
+        self.input_coin_trade.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
+        self.input_coin_trade.setInputMask("")
+        self.input_coin_trade.setText("")
+        self.input_coin_trade.setMaxLength(8)
+        self.input_coin_trade.setDragEnabled(False)
+        self.input_coin_trade.setReadOnly(False)
+        self.input_coin_trade.setClearButtonEnabled(False)
+        self.input_coin_trade.setObjectName("input_coin_trade")
+
+        self.example_trade = QtWidgets.QLabel(self.tab2)
+        self.example_trade.setGeometry(QtCore.QRect(50, 60, 111, 21))
+        self.example_trade.setObjectName("example_trade")
+
+        self.infobutton_trade = QtWidgets.QPushButton(self.tab2)
+        self.infobutton_trade.setGeometry(QtCore.QRect(70, 150, 101, 31))
+        self.infobutton_trade.setStyleSheet("")
+        self.infobutton_trade.setObjectName("infobutton_trade")
+        self.coin_price_trade = QtWidgets.QLabel(self.tab2)
+        self.coin_price_trade.setGeometry(QtCore.QRect(210, 60, 141, 31))
+        self.coin_price_trade.setText("")
+        self.coin_price_trade.setObjectName("coin_price_trade")
+
+        self.result_trade = QtWidgets.QLabel(self.tab2)
+        self.result_trade.setGeometry(QtCore.QRect(210, 120, 141, 31))
+        self.result_trade.setText("")
+        self.result_trade.setObjectName("result_trade")
+
+        self.input_coin_count_trade = QtWidgets.QLineEdit(self.tab2)
+        self.input_coin_count_trade.setGeometry(QtCore.QRect(60, 120, 121, 16))
+        self.input_coin_count_trade.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
+        self.input_coin_count_trade.setInputMask("")
+        self.input_coin_count_trade.setText("")
+        self.input_coin_count_trade.setMaxLength(8)
+        self.input_coin_count_trade.setDragEnabled(False)
+        self.input_coin_count_trade.setReadOnly(False)
+        self.input_coin_count_trade.setClearButtonEnabled(False)
+        self.input_coin_count_trade.setObjectName("input_coin_count_trade")
+
+        self.label_3 = QtWidgets.QLabel(self.tab2)
+        self.label_3.setGeometry(QtCore.QRect(0, 120, 57, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.tab2)
+        self.label_4.setGeometry(QtCore.QRect(20, 90, 41, 20))
+        self.label_4.setObjectName("label_4")
+
+        self.maintext_2 = QtWidgets.QLabel(self.tab2)
+        self.maintext_2.setGeometry(QtCore.QRect(40, 20, 141, 31))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.maintext_2.setFont(font)
+        self.maintext_2.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.maintext_2.setScaledContents(False)
+        self.maintext_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.maintext_2.setWordWrap(False)
+        self.maintext_2.setObjectName("maintext_2")
+        self.tabWidget.addTab(self.tab2, "")
+        self.tab3 = QtWidgets.QWidget()
+        self.tab3.setObjectName("tab3")
+
+        self.input_api_key_settings = QtWidgets.QLineEdit(self.tab3)
+        self.input_api_key_settings.setGeometry(QtCore.QRect(110, 30, 251, 21))
+        self.input_api_key_settings.setObjectName("input_api_key_settings")
+        self.api_key_settings = QtWidgets.QLabel(self.tab3)
+        self.api_key_settings.setGeometry(QtCore.QRect(50, 30, 57, 16))
+        self.api_key_settings.setObjectName("api_key_settings")
+        self.api_secret_key_settings = QtWidgets.QLabel(self.tab3)
+        self.api_secret_key_settings.setGeometry(QtCore.QRect(10, 60, 101, 16))
+        self.api_secret_key_settings.setObjectName("api_secret_key_settings")
+        self.input_api_secret_key_settings = QtWidgets.QLineEdit(self.tab3)
+        self.input_api_secret_key_settings.setGeometry(QtCore.QRect(110, 60, 251, 21))
+        self.input_api_secret_key_settings.setObjectName("input_api_secret_key_settings")
+        self.configure_button_settings = QtWidgets.QPushButton(self.tab3)
+        self.configure_button_settings.setGeometry(QtCore.QRect(110, 90, 121, 32))
+        self.configure_button_settings.setObjectName("configure_button_settings")
+        self.reset_button_settings = QtWidgets.QPushButton(self.tab3)
+        self.reset_button_settings.setGeometry(QtCore.QRect(240, 90, 121, 32))
+        self.reset_button_settings.setObjectName("reset_button_settings")
+
+        self.tabWidget.addTab(self.tab3, "")
         #MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 22))
@@ -54,48 +156,51 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         #MainWindow.setStatusBar(self.statusbar)
-        # self.action_XXXBTC = QtWidgets.QAction(MainWindow)
-        # self.action_XXXBTC.setObjectName("action_XXXBTC")
-        # self.action_XXXETH = QtWidgets.QAction(MainWindow)
-        # self.action_XXXETH.setObjectName("action_XXXETH")
-        # self.action_XXXBNB = QtWidgets.QAction(MainWindow)
-        # self.action_XXXBNB.setObjectName("action_XXXBNB")
-        # self.action_Cache_Reset = QtWidgets.QAction(MainWindow)
-        # self.action_Cache_Reset.setObjectName("action_Cache_Reset")
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
         self.infobutton.clicked.connect(self.infocoin_coin)
-        #self.infobutton.clicked.connect(self.label_2.clear)
+        self.infobutton_trade.clicked.connect(self.coin_price_trade.clear)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
 
     def infocoin_coin(self):
         while True:
             a=self.inputcoininfo.text()
             i=False
+            c=a
             if True==a.endswith('TL'):
                 turkish_liras=y.convert(1, 'USD', 'TRY')
                 a=a.replace("TL","")
-
                 a=a+"USDT"
-                print(a)
             try:
                 b=x.get_symbol_ticker(symbol=a)
             except:
                 self.label_2.setText("Error!!")
                 i=True
             if i==False:
-                self.label_2.setText(a+" : " + (str(float(b["price"])*float(turkish_liras))))
+                if True==c.endswith('TL'):
+                    self.label_2.setText(a+" : " + (str(float(b["price"])*float(turkish_liras))))
+                else:
+                    self.label_2.setText(a+" : " + (b["price"]))
             break
-
 
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Binance Bot"))
         self.infobutton.setText(_translate("MainWindow", "Coin Price"))
-        self.maintext.setText(_translate("MainWindow", "Binance Bot"))
         self.label.setText(_translate("MainWindow", "Example: HOTBTC"))
-        # self.action_XXXBTC.setText(_translate("MainWindow", " XXXBTC"))
-        # self.action_XXXETH.setText(_translate("MainWindow", " XXXETH"))
-        # self.action_XXXBNB.setText(_translate("MainWindow", " XXXBNB"))
-        # self.action_Cache_Reset.setText(_translate("MainWindow", " Cache Reset"))
+        self.maintext.setText(_translate("MainWindow", "Binance Bot"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab1), _translate("MainWindow", "Coin Price"))
+        self.example_trade.setText(_translate("MainWindow", "Example: HOTBTC"))
+        self.infobutton_trade.setText(_translate("MainWindow", "Coin Price"))
+        self.label_3.setText(_translate("MainWindow", "Amount :"))
+        self.label_4.setText(_translate("MainWindow", "Coin :"))
+        self.maintext_2.setText(_translate("MainWindow", "Market Buy"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab2), _translate("MainWindow", "Trade"))
+        self.api_key_settings.setText(_translate("MainWindow", "API Key :"))
+        self.api_secret_key_settings.setText(_translate("MainWindow", "API Secret Key :"))
+        self.configure_button_settings.setText(_translate("MainWindow", "Configure"))
+        self.reset_button_settings.setText(_translate("MainWindow", "Reset"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab3), _translate("MainWindow", "Settings"))
